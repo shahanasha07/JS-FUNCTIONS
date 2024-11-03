9841829da4dcee6b49adf36b004c898c2affc484
shahanasha07e <shahanashaa07@gmail.comm>
2024-11-03 19:35:55 +0530

    basic level

---
+++ greeting.js
@@ -0,0 +1,14 @@
+// function greeting(name){
+//     console.log("good morning "+ name)
+// }
+// greeting("shahana");
+// greeting("shifana");
+
+
+
+// hello world
+function msg(){
+    console.log("Hello world")
+}
+msg();
+
