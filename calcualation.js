add73698312c3fe707759c9b5e205252b9e80997
shahanasha07e <shahanashaa07@gmail.comm>
2024-11-04 16:30:58 +0530

    calculation

---
+++ calcualation.js
@@ -0,0 +1,28 @@
+// function sum(a,b){
+//    return (a+b)
+// }
+
+//  var ressult=sum(30,3)
+//  console.log(ressult)
+
+
+
+//square of a number
+// function sqr(a){
+//     console.log(a*a)
+// }
+// sqr(5);
+ 
+
+// area of a rectangle
+// function area(a,b){
+//     console.log(a*b)
+// }
+// area(2,5)
+
+
+// convert Celsius to Fahrenheit?
+function celcius(c){
+    console.log((c * 9/5) + 32);
+}
+celcius(5);
